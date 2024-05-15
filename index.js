bf4c989ef3ec1573ad0d99ee4de62f72415d34ca
Brian Carela <BrianCarela@gmail.com>
2024-05-15 16:38:01 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const serverless = require('serverless-http')
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'success'
+    })
+})
+
+app.get('/test', (req, res) => {
+    res.json({
+        message: 'test success'
+    })
+})
+
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`lambda test server running`)
+})
+
+// module.exports.handler = serverless(app)
